7adf2abf67d473e661c6c3bc1a8276cb7fa7d475
Facundo Lerena <faculerena@gmail.com>
2025-07-24 11:04:21 -0300

    [linter] Added `needless_return` lint and tests

--- third_party/move/tools/move-linter/src/model_ast_lints.rs
+++ third_party/move/tools/move-linter/src/model_ast_lints.rs
@@ -9,6 +9,7 @@ mod needless_bool;
 mod needless_deref_ref;
 mod needless_ref_deref;
 mod needless_ref_in_field_access;
+mod needless_return;
 mod nonminimal_bool;
 mod self_assignment;
 mod simpler_numeric_expression;
@@ -24,6 +25,7 @@ pub fn get_default_linter_pipeline() -> Vec<Box<dyn ExpChecker>> {
         Box::<almost_swapped::AlmostSwapped>::default(),
         Box::<blocks_in_conditions::BlocksInConditions>::default(),
         Box::<needless_bool::NeedlessBool>::default(),
+        Box::<needless_return::NeedlessReturn>::default(),
         Box::<needless_ref_in_field_access::NeedlessRefInFieldAccess>::default(),
         Box::<needless_deref_ref::NeedlessDerefRef>::default(),
         Box::<needless_ref_deref::NeedlessRefDeref>::default(),
